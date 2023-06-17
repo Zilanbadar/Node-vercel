b6e980d45fd7bc1fd864c61640068897ef770ef7
Zilan Badar <80773224+Zilanbadar@users.noreply.github.com>
2023-06-17 19:42:48 +0500

    Update server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 //port config
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 9002;
 
 //db config for APIs
 await connect();
